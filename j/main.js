0c463a4aa905de3528cab1fd2bf96f0e0ab2282b
sofia <selizondo003@insite.4cd.edu>
2022-05-20 12:28:42 -0700

    beginning to add in dexie

--- j/main.js
+++ j/main.js
@@ -1,3 +1,32 @@
+//create new indexDB database
+var db = new Dexie("QuotesDatabase");
+
+//define the database schema(structure), which includes tables and their key indiecs
+db.version(1).stores({
+    txtquote: `id, quote`,
+    author: `id, author`
+});
+
+//populate quotes
+await db.txtquote.bulkPut([
+
+])
+//populate author
+await db.txtquote.bulkPut([
+
+])
+await db
+.transaction("rw", db.txtquote, db.author, async (tx) => {
+    let info =[];
+
+    //query the db and convert results into an easy to use array.
+    //get all of the records in the quote table.
+    info[0] = await db.txtquote.toArray();
+
+    
+}
+
+
 //pulling in a random inspirational quote 
 const inspirationalQuote = ()  => {
     fetch("https://api.quotable.io/random")
