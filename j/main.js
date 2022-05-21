e61e07527eb82297fa0ebcc8a5891f332ce26d58
sofia <selizondo003@insite.4cd.edu>
2022-05-21 01:47:59 -0700

    validation and beauitfy

--- j/main.js
+++ j/main.js
@@ -1,45 +1,45 @@
-(async function (){
+(async function () {
     "use strict";
 
     let poet_list = document.querySelector(".poets");
     //let poem_list = document.querySelector(".poems");
 
-//create new indexDB database
-var db = new Dexie("PoetryDatabase");
+    //create new indexDB database
+    var db = new Dexie("PoetryDatabase");
 
-//define the database schema(structure), which includes tables and their key indiecs
-db.version(1).stores({
-    poet: `++, &authorPoet`,
-    //poems: `++, &titleName`
-});
+    //define the database schema(structure), which includes tables and their key indiecs
+    db.version(1).stores({
+        poet: `++, &authorPoet`,
+        //poems: `++, &titleName`
+    });
 
 
-//go get author data from poets
-const poet_data = await fetch('https://poetrydb.org/authors');
-const poet = await poet_data.json();
-const poet_array = poet.authors;
+    //go get author data from poets
+    const poet_data = await fetch('https://poetrydb.org/authors');
+    const poet = await poet_data.json();
+    const poet_array = poet.authors;
 
-/*this will come at a later phase
-//go get poem data
-const poem_data = await fetch('https://poetrydb.org/title');
-const poem = await poem_data.json();
-const poem_array = poem.titles;
-*/
+    /*this will come at a later phase
+    //go get poem data
+    const poem_data = await fetch('https://poetrydb.org/title');
+    const poem = await poem_data.json();
+    const poem_array = poem.titles;
+    */
 
-// populate the tables
-db.poet.bulkPut(poet_array);
-   //db.poems.bulkPut(poem_array);
+    // populate the tables
+    db.poet.bulkPut(poet_array);
+    //db.poems.bulkPut(poem_array);
 
-// make a queries of the database
-const folks =  await db.poet.toArray();
-   //const composition = await db.poems.toArray();
- 
-//creating dropdown of the different authors
+    // make a queries of the database
+    const folks = await db.poet.toArray();
+    //const composition = await db.poems.toArray();
+
+    //creating dropdown of the different authors
     folks.forEach((author) => {
-      const option = document.createElement("OPTION");
-      option.textContent = author;
-      option.value = author;
-      poet_list.append(option);
+        const option = document.createElement("OPTION");
+        option.textContent = author;
+        option.value = author;
+        poet_list.append(option);
     });
 
     /* 
@@ -50,7 +50,7 @@ const folks =  await db.poet.toArray();
     });
     */
 
-    
+
     //listening for changes on list 
     //getting last name of the poet
     poet_list.addEventListener("change", async (e) => {
@@ -60,63 +60,64 @@ const folks =  await db.poet.toArray();
 
         console.log(lastName);
 
-//getting 1st poem from API
+        //getting 1st poem from API
         const poeminfo = await fetch(`https://poetrydb.org/author,poemcount/${lastName};1`);
         const currentPoem = await poeminfo.json();
         const poemText = currentPoem[0].lines.join("\n");
         const titleText = currentPoem[0].title;
-        
+
         //make sure display is empty 
         let poem = document.getElementById("poem");
         poem.innerHTML = '';
+        let title = document.getElementById("title");
         title.innerHTML = '';
 
         //render title of poem
-        title.innerHTML += `<p> </p>`
+        title.innerHTML += `<p> </p>`;
         title.append(titleText);
 
         //render the poem 
-        poem.innerHTML += `<p>  </p>`
+        poem.innerHTML += `<p>  </p>`;
         poem.append(poemText);
         console.log(poemText);
 
 
-        
 
-    })
-  
-} ()); // end IIFE
+
+    });
+
+}()); // end IIFE
 
 
 
 //pulling in a random inspirational quote 
-const inspirationalQuote = ()  => {
+const inspirationalQuote = () => {
     fetch("https://api.quotable.io/random")
         .then(response => {
             return response.json();
         })
         .then(response => {
-            document.getElementById("quote").innerHTML = response.content; 
+            document.getElementById("quote").innerHTML = response.content;
             document.getElementById("author").innerHTML = "- " + response.author;
         })
-      .catch(function(err) {
+        .catch(function (err) {
             console.log("Error", err);
         });
-    }
-    inspirationalQuote();
+};
+inspirationalQuote();
 
 
 //pulling in a random dog image or video
-const randomImg =() =>{
+const randomImg = () => {
     fetch("https://random.dog/woof.json")
         .then(data => {
             return data.json();
         })
         .then(data => {
-            const imgElement = document.getElementById("imgElement")
-            imgElement.src=data.url;
+            const imgElement = document.getElementById("imgElement");
+            imgElement.src = data.url;
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log("Error", err);
         });
 };
